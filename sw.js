03429426b7d0c354002151b4a0bc64ff6e1e7d08
jcarnahan-hub <jcarnahan78@gmail.com>
2026-04-12 17:11:29 -0400

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,32 @@
+const CACHE_NAME = 'aurelia-echoes-v1';
+const ASSETS = [
+  '/aureliaechoes/',
+  '/aureliaechoes/index.html',
+  '/aureliaechoes/style.css',
+  '/aureliaechoes/app.js',
+  '/aureliaechoes/firebase.js',
+  '/aureliaechoes/db.js',
+  '/aureliaechoes/import.js',
+  '/aureliaechoes/manifest.json',
+  '/aureliaechoes/icon.png'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
+  );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
+    )
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(cached => cached || fetch(event.request))
+  );
+});
